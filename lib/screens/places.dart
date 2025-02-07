ee5a74f6f59a68f34849e30073539b6e421aea1d
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-02-07 20:44:44 +0700

    feat: add "AddPlaceScreen"

---
+++ lib/screens/places.dart
@@ -0,0 +1,26 @@
+import 'package:favorite_places_app/screens/add_place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (ctx) => const AddPlaceScreen(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.add))
+        ],
+      ),
+    );
+  }
+}
